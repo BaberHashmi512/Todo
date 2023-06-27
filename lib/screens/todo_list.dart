5eb5238042793717a8ee6ac6b9250ac2a9ec2414
Baber hashmi <baberhashmi512@gmail.com>
2023-06-27 19:32:10 +0500

    Internet connectivity

--- lib/screens/todo_list.dart
+++ lib/screens/todo_list.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:todo/services/todo_service.dart';
+import 'package:todo/sql_helper.dart';
 import 'package:todo/utils/snackbar_helper.dart';
 import 'package:todo/widgets/todo_card.dart';
 import 'add_page.dart';
+import 'dart:async';
+import 'dart:developer' as developer;
+
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/services.dart';
 
 class TodoListPage extends StatefulWidget {
   const TodoListPage({Key? key}) : super(key: key);
@@ -12,13 +18,40 @@ class TodoListPage extends StatefulWidget {
 }
 
 class _TodoListPageState extends State<TodoListPage> {
+  ConnectivityResult _connectionStatus = ConnectivityResult.none;
+  final Connectivity _connectivity = Connectivity();
+  late StreamSubscription<ConnectivityResult> _connectivitySubscription;
+
+
+  @override
+  void dispose() {
+    _connectivitySubscription.cancel();
+    super.dispose();
+  }
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+
+  List<Map<String,dynamic>> _journals =[];
   bool isLoading = false;
+  void _refreshJournals() async{
+    final data = await SQLHelper.getItems();
+    setState(() {
+      _journals =data;
+      isLoading =false;
+    });
+  }
   List items = [];
 
   @override
   void initState() {
     super.initState();
+    initConnectivity();
+
+    _connectivitySubscription =
+        _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
     fetchTodo();
+    _refreshJournals();
+    print("..number of items ${_journals.length}");
   }
 
   @override
@@ -111,5 +144,42 @@ class _TodoListPageState extends State<TodoListPage> {
   void showSuccessMessage(String message) {
     final snackBar = SnackBar(content: Text(message));
     ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+  void _showForm(int? id)async{
+    if (id != null){
+      final existingJournal =
+          _journals.firstWhere((element) => element['id']==id);
+      titleController.text=existingJournal['title'];
+      descriptionController.text=existingJournal['description'];
+    }
+  }
+  Future<void> _addItem() async{
+    await SQLHelper.createItem(
+        titleController.text,
+        descriptionController.text,
+    );
+    _refreshJournals();
+    print("..number of items ${_journals.length}");
+  }
+  Future<void> initConnectivity() async {
+    late ConnectivityResult result;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      result = await _connectivity.checkConnectivity();
+    } on PlatformException catch (e) {
+      developer.log('Could\'t check connectivity status', error: e);
+      return;
+    }
+    if (!mounted) {
+      return Future.value(null);
+    }
+
+    return _updateConnectionStatus(result);
+  }
+
+  Future<void> _updateConnectionStatus(ConnectivityResult result) async {
+    setState(() {
+      _connectionStatus = result;
+    });
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import connectivity_macos
+import connectivity_plus
+import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
+  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
